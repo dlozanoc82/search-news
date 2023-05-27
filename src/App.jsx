648b47bf546840da1dc816df26318ac82f076da7
Daniel <dlozanoc862@gmail.com>
2023-05-27 13:55:08 -0500

    [ADD] The context news provider is created

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,31 @@
 
 import { Container, Grid, Typography } from "@mui/material"
 import SearchForm from "./components/SearchForm"
+import { NewsProvider } from "./context/newsProvider"
+
 
 function App() {
     return (
-        <Container>
-            <header>
-                <Typography align="center" marginY={5} component="h1" variant="h3">
-                    Search News 
-                </Typography>
-            </header>
+        <NewsProvider>
+            <Container>
+                <header>
+                    <Typography align="center" marginY={5} component="h1" variant="h3">
+                        Search News 
+                    </Typography>
+                </header>
 
-            <Grid
-                container
-                direction="row"
-                justifyContent="center"
-                alignItems="center"
-            >
-                <Grid item xs={12} md={6} lg={4}>
-                    <SearchForm />
+                <Grid
+                    container
+                    direction="row"
+                    justifyContent="center"
+                    alignItems="center"
+                >
+                    <Grid item xs={12} md={6} lg={4}>
+                        <SearchForm />
+                    </Grid>
                 </Grid>
-            </Grid>
-        </Container>
+            </Container>
+        </NewsProvider>
     )
 }
 
